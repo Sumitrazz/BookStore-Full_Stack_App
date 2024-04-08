89f839a9af66b87ec8446dd833e8678a248497d4
sumit kumar <sumitkumar@sumits-MacBook-Air.local>
2024-04-08 18:31:44 +0530

    add: initial files

---
+++ backend/config.js
@@ -0,0 +1,2 @@
+export const PORT = 5555;
+export const mongoDBURL = 'mongodb://Sumitk:sumit12345@ac-i5ysma7-shard-00-00.xfxuce2.mongodb.net:27017,ac-i5ysma7-shard-00-01.xfxuce2.mongodb.net:27017,ac-i5ysma7-shard-00-02.xfxuce2.mongodb.net:27017/?ssl=true&replicaSet=atlas-10kpqg-shard-0&authSource=admin&retryWrites=true&w=majority&appName=books'
